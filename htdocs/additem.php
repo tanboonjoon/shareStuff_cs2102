dbbd87359256adf81f18d95ebe7a019cad9d0db1
tanboonjoon <tbj_999@hotmail.com>
2016-09-29 02:24:09 +0800

    modify format

--- htdocs/additem.php
+++ htdocs/additem.php
@@ -6,17 +6,17 @@
 </head>
 
 <body>
-<table>
-<tr> <td colspan="2" style="background-color:#FFA500;">
-<h1> Add Item for Lending</h1>
-</td> </tr>
 
-<?php
-session_start();
-include_once 'dbconnect.php';
-?>
+  <tr> <td colspan="2" style="background-color:#FFA500;">
+    <h1> Add Item for Lendings</h1>
+  </td> </tr>
 
-<nav class="navbar navbar-default" role= "navigation">
+  <?php
+  session_start();
+  include_once 'dbconnect.php';
+  ?>
+
+  <nav class="navbar navbar-default" role= "navigation">
    <div class="container-fluid">
     <div class="navbar-header>">
      <button type="button" class ="navbar-toggle" data-toggle="collapse" data-target="#navbar1">
@@ -44,73 +44,84 @@ include_once 'dbconnect.php';
   </div>
 </nav>
 
-<tr>
-<td style="background-color:#eeeeee;">
-<div class="container">
-<form>
-        Item Name <input type="text" name="ItemName" id="ItemName"> <br>
-
-        Description <input type="text" name="Description" id="Description"> <br>
-
-        Category <select required name="Category"> <option value="">Select Category</option>
-
-            <option value= "Tools & Gardening" > Tools & Gardening</option>
-            <option value= "Sport & Outdoors" > Sport & Outdoors</option>
-            <option value= "Parties & Events" > Parties & Events</option>
-            <option value= "Apparel & Accessories" > Apparel & Accessories</option>
-            <option value= "Kids & Baby" > Kids & Baby</option>
-            <option value= "Electronic" > Electronic</option>
-            <option value= "Movies, Music, Book & Games" > Movies, Music, Book & Games</option>
-            <option value= "Motor Vehicles" >Motor Vehicles </option>
-            <option value= "Arts and Crafts" >Arts and Crafts </option>
-            <option value= "Home & Appliances" > Home & Appliances</option>
-            <option value= "Office & Education" > Office & Education</option>
-            <option value= "Spaces & Venues" > Spaces & Venues</option>
-            <option value= "Other" > Other</option>
 
 
-        </select><br>
-
-        Return Instruction <input type="text" name="ReturnInstruction" id="ReturnInstruction"> <br>
-
-        Pick Up Instruction <input type="text" name="PickUpInstruction" id="PickUpInstruction"> <br>
-
-        <input type="radio" name="BidType" id="BidType1" value="free" checked="">free 
-        <input type="radio" name="BidType" id="BidType2" value="require fee">with fee
-        <br>
-        <input type="submit" name="formSubmit" value="Add" >
-</form>
-</div>
-<?php
-
-if(isset($_GET['formSubmit'])) 
-{
-    $query = "INSERT INTO item(item_name, owner, description, category,return_instruction,pickup_instruction, availability, bid_type) VALUES 
-             ('".$_GET['ItemName']."',
-              '{$_SESSION['usr_email']}',
-              '".$_GET['Description']."',
-              '".$_GET['Category']."',
-              '".$_GET['ReturnInstruction']."',
-              '".$_GET['PickUpInstruction']."',
-              true,
-              '".$_GET['BidType']."')";
-
-    echo "<b>SQL:   </b>".$query."<br><br>";
-    pg_query($query) or die('Query failed: ' . pg_last_error());
-}
-?>
-
-</td> </tr>
-<?php
-pg_close($dbconn);
-?>
-<tr>
-<td colspan="2" style="background-color:#FFA500; text-align:center;"> Copyright &#169; CS2102
-</td> </tr>
-</table>
+<table>
 
 
-<script src="js/jquery-1.10.2.js"></script>
-<script src="js/bootstrap.min.js"></script>    
+  <tr>
+    <td style="background-color:#eeeeee;">
+      <div class="container-fluid">
+        <div class ="row">
+          <div class="col-md-6">
+
+
+            <form>
+              Item Name <input type="text" name="ItemName" id="ItemName"> <br>
+
+              Description <input type="text" name="Description" id="Description"> <br>
+
+              Category <select required name="Category"> <option value="">Select Category</option>
+
+              <option value= "Tools & Gardening" > Tools & Gardening</option>
+              <option value= "Sport & Outdoors" > Sport & Outdoors</option>
+              <option value= "Parties & Events" > Parties & Events</option>
+              <option value= "Apparel & Accessories" > Apparel & Accessories</option>
+              <option value= "Kids & Baby" > Kids & Baby</option>
+              <option value= "Electronic" > Electronic</option>
+              <option value= "Movies, Music, Book & Games" > Movies, Music, Book & Games</option>
+              <option value= "Motor Vehicles" >Motor Vehicles </option>
+              <option value= "Arts and Crafts" >Arts and Crafts </option>
+              <option value= "Home & Appliances" > Home & Appliances</option>
+              <option value= "Office & Education" > Office & Education</option>
+              <option value= "Spaces & Venues" > Spaces & Venues</option>
+              <option value= "Other" > Other</option>
+
+
+            </select><br>
+
+            Return Instruction <input type="text" name="ReturnInstruction" id="ReturnInstruction"> <br>
+
+            Pick Up Instruction <input type="text" name="PickUpInstruction" id="PickUpInstruction"> <br>
+
+            <input type="radio" name="BidType" id="BidType1" value="free" checked="">free 
+            <input type="radio" name="BidType" id="BidType2" value="require fee">with fee
+            <br>
+            <input type="submit" name="formSubmit" value="Add" >
+          </form>
+        </div>
+      </div>
+    </div>
+    <?php
+
+    if(isset($_GET['formSubmit'])) 
+    {
+      $query = "INSERT INTO item(item_name, owner, description, category,return_instruction,pickup_instruction, availability, bid_type) VALUES 
+      ('".$_GET['ItemName']."',
+      '{$_SESSION['usr_email']}',
+      '".$_GET['Description']."',
+      '".$_GET['Category']."',
+      '".$_GET['ReturnInstruction']."',
+      '".$_GET['PickUpInstruction']."',
+      true,
+      '".$_GET['BidType']."')";
+
+      echo "<b>SQL:   </b>".$query."<br><br>";
+      pg_query($query) or die('Query failed: ' . pg_last_error());
+    }
+    ?>
+
+  </td> </tr>
+  <?php
+  pg_close($dbconn);
+  ?>
+  <tr>
+    <td colspan="2" style="background-color:#FFA500; text-align:center;"> Copyright &#169; CS2102
+    </td> </tr>
+  </table>
+
+
+  <script src="js/jquery-1.10.2.js"></script>
+  <script src="js/bootstrap.min.js"></script>    
 </body>
 </html>
--- htdocs/search-item.php
+++ htdocs/search-item.php
@@ -6,29 +6,28 @@
 </head>
 
 <body>
-    <table>
-        <tr> <td colspan="2" style="background-color:#FFA500;">
-            <h1> Online Item Catalog</h1>
-        </td> </tr>
+    <tr> <td colspan="2" style="background-color:#FFA500;">
+        <h1> Online Item Catalog</h1>
+    </td> </tr>
 
-        <?php
-        session_start();
-        include_once 'dbconnect.php';
-        ?>
+    <?php
+    session_start();
+    include_once 'dbconnect.php';
+    ?>
 
-        <nav class="navbar navbar-default" role= "navigation">
-           <div class="container-fluid">
-            <div class="navbar-header>">
-             <button type="button" class ="navbar-toggle" data-toggle="collapse" data-target="#navbar1">
+    <nav class="navbar navbar-default" role= "navigation">
+     <div class="container-fluid">
+        <div class="navbar-header>">
+           <button type="button" class ="navbar-toggle" data-toggle="collapse" data-target="#navbar1">
               <span class="sr-only">Toggle navigation</span>
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
-            </button>
+          </button>
 
-          </div>
-          <div class="collapse navbar-collapse" id="navbar1">
-           <ul class="nav navbar-nav navbar-right">
+      </div>
+      <div class="collapse navbar-collapse" id="navbar1">
+         <ul class="nav navbar-nav navbar-right">
             <?php if (isset($_SESSION['usr_email'])) { ?>
               <li><p class="navbar-text">Signed in as <?php echo $_SESSION['usr_name']; ?> </p></li>
               <li><a href="index.php">Home</a></li>
@@ -38,95 +37,107 @@
                 <li><a href="login.php">Log In</a></li>
                 <li><a href="signup.php">Sign Up</a></li>
                 <?php } ?>
-              </ul>
-            </div>
-          </div>
-        </nav>
-
-        <tr>
-            <td style="background-color:#eeeeee;">
-                <form>
-                    Item Name <input type="text" name="Keyword" id="Keyword" required class="form-control">
-
-                    <select name="Category"> <option value="All">All</option>
-
-                        <option value= "Tools & Gardening" > Tools & Gardening</option>
-                        <option value= "Sport & Outdoors" > Sport & Outdoors</option>
-                        <option value= "Parties & Events" > Parties & Events</option>
-                        <option value= "Apparel & Accessories" > Apparel & Accessories</option>
-                        <option value= "Kids & Baby" > Kids & Baby</option>
-                        <option value= "Electronic" > Electronic</option>
-                        <option value= "Movies, Music, Book & Games" > Movies, Music, Book & Games</option>
-                        <option value= "Motor Vehicles" >Motor Vehicles </option>
-                        <option value= "Arts and Crafts" >Arts and Crafts </option>
-                        <option value= "Home & Appliances" > Home & Appliances</option>
-                        <option value= "Office & Education" > Office & Education</option>
-                        <option value= "Spaces & Venues" > Spaces & Venues</option>
-                        <option value= "Other" > Other</option>
+            </ul>
+        </div>
+    </div>
+</nav>
 
 
-                    </select>
+<table>
 
-                    <input type="radio" name="BidType" id="BidType1" value="free" checked>free
-                    <input type="radio" name="BidType" id="BidType2" value="require fee">require fee
 
-                    <input type="submit" name="formSubmit" value="Search" >
-                </form>
-                <?php
+    <tr>
+        <td style="background-color:#eeeeee;">
+            <div class = "container-fluid">
+                <div class="row">
+                    <div class="col-md-6">
+                        <form>
+                            Item Name <input type="text" name="Keyword" id="Keyword" required class="form-control">
 
-                if(isset($_GET['formSubmit'])) 
-                {   
+                            <select name="Category"> <option value="All">All</option>
 
-                    if(strcasecmp("All", $_GET['Category']) == 0) {
-                        $query =  "SELECT * FROM item WHERE item_name like '%".$_GET['Keyword']."%' AND  bid_type='".$_GET['BidType']."' AND availability = true";
-                    } else {
-                        $query = "SELECT * FROM item WHERE item_name like '%".$_GET['Keyword']."%' AND category='".$_GET['Category']."' AND bid_type='".$_GET['BidType']."' AND availability = true ";
-                    }
+                                <option value= "Tools & Gardening" > Tools & Gardening</option>
+                                <option value= "Sport & Outdoors" > Sport & Outdoors</option>
+                                <option value= "Parties & Events" > Parties & Events</option>
+                                <option value= "Apparel & Accessories" > Apparel & Accessories</option>
+                                <option value= "Kids & Baby" > Kids & Baby</option>
+                                <option value= "Electronic" > Electronic</option>
+                                <option value= "Movies, Music, Book & Games" > Movies, Music, Book & Games</option>
+                                <option value= "Motor Vehicles" >Motor Vehicles </option>
+                                <option value= "Arts and Crafts" >Arts and Crafts </option>
+                                <option value= "Home & Appliances" > Home & Appliances</option>
+                                <option value= "Office & Education" > Office & Education</option>
+                                <option value= "Spaces & Venues" > Spaces & Venues</option>
+                                <option value= "Other" > Other</option>
 
-                    echo "<b>Search result for your item <br><br>";
-                    $result = pg_query($query) or die('Query failed: ' . pg_last_error());
-                    echo "<table border=\"1\" >
-                    <tr>
-                        <th>ID</th>
-                        <th>Item</th>
-                        <th>Owner</th>
-                        <th>Description</th>
-                        <th>Category</th>
-                        <th>Pick up instruction</th>
-                        <th>Return instruction</th>
-                        <th>Bid type</th>
-                    </tr>";
 
-                    if(pg_num_rows($result) == 0) {
-                        echo "<tr><td align='center' colspan ='8'> No one is currently trying to lend out the item you are searching for </td></tr>";
-                    }
-                    while ($row = pg_fetch_row($result)){
-                      echo "<tr>";
-                      $arrlength = count($row);
-                      for ($i = 0; $i < $arrlength; $i++) {
-                        if($i == 7) {
+                            </select>
+                            <label class="radio-inline">
+                                <input type="radio" name="BidType" id="BidType1" value="free" checked>free 
+                            </label>
+                            <label class="radio-inline">
+                                <input type="radio" name="BidType" id="BidType2" value="require fee">require fee
+                            </label>
+                            <input type="submit" name="formSubmit" value="Search" >
+                        </form>
+                        <?php
 
-                        }else {
-                            echo "<td>" . $row[$i] . "</td>";
+                        if(isset($_GET['formSubmit'])) 
+                        {   
+
+                            if(strcasecmp("All", $_GET['Category']) == 0) {
+                                $query =  "SELECT * FROM item WHERE item_name like '%".$_GET['Keyword']."%' AND  bid_type='".$_GET['BidType']."' AND availability = true";
+                            } else {
+                                $query = "SELECT * FROM item WHERE item_name like '%".$_GET['Keyword']."%' AND category='".$_GET['Category']."' AND bid_type='".$_GET['BidType']."' AND availability = true ";
+                            }
+
+                            echo "<b>Search result for your item <br><br>";
+                            $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+                            echo "<table border=\"1\" >
+                            <tr>
+                                <th>ID</th>
+                                <th>Item</th>
+                                <th>Owner</th>
+                                <th>Description</th>
+                                <th>Category</th>
+                                <th>Pick up instruction</th>
+                                <th>Return instruction</th>
+                                <th>Bid type</th>
+                            </tr>";
+
+                            if(pg_num_rows($result) == 0) {
+                                echo "<tr><td align='center' colspan ='8'> No one is currently trying to lend out the item you are searching for </td></tr>";
+                            }
+                            while ($row = pg_fetch_row($result)){
+                              echo "<tr>";
+                              $arrlength = count($row);
+                              for ($i = 0; $i < $arrlength; $i++) {
+                                if($i == 7) {
+
+                                }else {
+                                    echo "<td>" . $row[$i] . "</td>";
+                                }
+
+                            }
+                            echo "</tr>";
                         }
+                        echo "</table>";
 
+                        pg_free_result($result);
                     }
-                    echo "</tr>";
-                }
-                echo "</table>";
+                    ?>
 
-                pg_free_result($result);
-            }
-            ?>
+                </td> </tr>
+                <?php
+                pg_close($dbconn);
+                ?>
+                <tr>
+                    <td colspan="2" style="background-color:#FFA500; text-align:center;"> Copyright &#169; CS2102
+                    </td> </tr>
+                </table>
+            </div>
+        </div>
+    </div>
 
-        </td> </tr>
-        <?php
-        pg_close($dbconn);
-        ?>
-        <tr>
-            <td colspan="2" style="background-color:#FFA500; text-align:center;"> Copyright &#169; CS2102
-            </td> </tr>
-        </table>
-
-    </body>
-    </html>
+</body>
+</html>
